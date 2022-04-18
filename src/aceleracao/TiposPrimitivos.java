74b5b2f53b80cef6025b89d46e91e6407cc81da0
alexandrexpj <alexandrexpjr@hotmail.com>
2022-04-18 20:01:58 -0300

    feat: add more primitive types

--- src/aceleracao/TiposPrimitivos.java
+++ src/aceleracao/TiposPrimitivos.java
@@ -24,6 +24,35 @@ public class TiposPrimitivos {
 		System.out.println(soma);
 		System.out.println(bigInt);
 		
+		// long type are numeric variables in range (-9.223.372.036.854.775.808, 9.223.372.036.854.775.807)
+		long numA = 102040; 
+        long numB = 102030405060L; // adicionado L no final para indicar que estamos inserindo um valor do tipo long 
+        System.out.println(numA); 
+        System.out.println(numB);
+        
+        // float type are numeric variables in range (-1,4024E-37, 3,40282347E+38)
+        float floatA = -101.23f; // adicionado f no final para indicar que estamos inserindo um valor do tipo float
+        float floatB = 2.356f;
+        System.out.println(floatA + floatB); // -98.874
+        
+        // double type are numeric variables in range (-4,9E-324, 1,7976931348623157E308)
+        double varDouble = 2.356; // double inicializado naturalmente 
+        double doubleRecebendoFloat = 1.409F; // double aceitando número float 
+        double doubleRecebendoLong = 12930L; // double aceitando número long 
+        System.out.println(doubleRecebendoFloat + varDouble); // 3.765000039100647
+        System.out.println(doubleRecebendoLong); // 12930.0 
+        
+        // boolean
+        
+        boolean isBoolean = true; // outros exemplos: isGreaterThan, isFlexible, isStarted 
+        System.out.println(isBoolean);
+        
+        // char: one character
+        char letraA = 'a'; 
+        char letraAMaiuscula = 'A'; 
+        System.out.println(letraA); // a 
+        System.out.println(letraAMaiuscula); // A
+        System.out.println(letraAMaiuscula == letraA); // false
 	}
 
 }
